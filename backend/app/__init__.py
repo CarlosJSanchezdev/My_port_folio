0a81979486fbb2d0fdb30dea434e77088a7260ed
CarlosJSanchezdev <cjsatlas@hotmail.com>
2026-01-31 23:51:31 -0500

    feat: Add 'https://my-port-folio-n6vb.onrender.com' to CORS allowed origins.

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -43,7 +43,7 @@ def create_app(config_class=Config):
                 "https://127.0.0.1:4200",
                 "http://localhost:4000",
                 "http://127.0.0.1:4000",
-                "https://carlosjsanchezdev.vercel.app"
+                "https://carlosjsanchezdev.vercel.app",
                 "https://my-port-folio-n6vb.onrender.com"
             ],
             "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
